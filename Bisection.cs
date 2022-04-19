630098666cf89c7af9737b65eb430447a1523cb2
souravkunduss1@outlook.com <souravkunduss1@outlook.com>
2022-04-19 15:27:46 +0600

    Add project files.

---
+++ Bisection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bisection
+{
+    public class Bisection
+    {
+        public double acc = 0.0001;
+        public double c;
+
+        public double Func(double x)
+        {
+            return x * x * x - x - 1;
+        }
+
+        public void FindRoot(double a, double b)
+        {
+            if(a<b && Func(a)*Func(b)<0)
+            {
+                while(true)
+                {
+                    c = (a + b) / 2;
+                    if(Math.Abs(Func(c))<=acc)
+                    {
+                        Console.WriteLine("Root = {0:0.0000}", c);
+                        break;
+                    }
+                    else
+                    {
+                        if(Func(a)*Func(c)<0)
+                        {
+                            b = c;
+                        }
+                        else
+                        {
+                            a = c;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bisection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1, b = 2;
+            var root = new Bisection();
+            root.FindRoot(a, b);
+        }
+    }
+}
